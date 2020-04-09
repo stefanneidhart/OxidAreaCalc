5b258af5b8f93268d459b5433368d3bd9638d82b
stefanneidhart <56339117+stefanneidhart@users.noreply.github.com>
2020-04-09 11:42:27 +0200

    Update Basket.php

--- Model/Basket.php
+++ Model/Basket.php
@@ -41,6 +41,9 @@ class Basket extends Basket_parent {
 		//$areacalc_active = oxConfig::getParameter('areacalc_active');
 		$calcnewflag = false;
 		if (!empty($areacalc_active) && $areacalc_active == '1') {
+		    
+		
+		    
 			$aPersParam['areacalc_active'] = '1';
 			$aPersParam['breite'] = \OxidEsales\Eshop\Core\Registry::getConfig()->getRequestParameter("breite");
 			$aPersParam['hoehe'] = \OxidEsales\Eshop\Core\Registry::getConfig()->getRequestParameter("hoehe");
@@ -101,6 +104,7 @@ class Basket extends Basket_parent {
 			//inserting new
 			$oBasketItem = oxNew('oxbasketitem');
 			try {
+			    var_dump($aPersParam);
 				$oBasketItem->setStockCheckStatus($this->getStockCheckMode());
 				$oBasketItem->init($sProductID, $dAmount, $aSel, $aPersParam, $blBundle);
 				if ($calcnewflag) {
